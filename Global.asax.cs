6125297bc0f7bdde6b37ab654caf5919fd18b33d
Patrick Pointdujour <ppointdujour@gmail.com>
2012-09-01 15:35:50 -0400

    Chapter 1 done

--- Global.asax.cs
+++ Global.asax.cs
@@ -1,9 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using System.Web.Routing;
+
+using ContosoUniversity.Models;
+using ContosoUniversity.DAL;
 
 namespace ContosoUniversity
 {
@@ -35,6 +39,8 @@ namespace ContosoUniversity
 
             RegisterGlobalFilters(GlobalFilters.Filters);
             RegisterRoutes(RouteTable.Routes);
+
+            Database.SetInitializer<SchoolContext>(new SchoolInitializer());
         }
     }
 }
